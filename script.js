f6b9d11056d0d96091581564b9ee932601cb5760
P.Laszlo <70635967+PLACKO135@users.noreply.github.com>
2023-12-05 09:55:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const day = document.querySelectorAll(".day");
+const d = new Date();
+const da = d.getDate();
+let yippe=document.getElementById("placeholder");
+day.forEach(element => {
+    element.addEventListener("click", function () {
+        if (parseInt(element.innerHTML) <= da) {
+
+            yippe.style.backgroundImage=`none`;
+            if (element.classList.contains("active")) {
+                element.innerHTML = 'Kinyitva';
+               
+            } else {
+                element.classList.add("active");
+                element.innerHTML = '<img src="Gragas_3.png"/>';
+            }
+        }
+        else{
+            //yippe.style.backgroundImage=`url('frieren.png')`;
+            alert('NEM nyithatod ki még');
+        }
+    });
+})
